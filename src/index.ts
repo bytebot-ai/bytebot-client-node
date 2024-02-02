08290fca7dec889cf6a28079567d6af339fd0e7c
Carl Atupem <carl@atupem.com>
2024-02-02 10:57:28 -0800

    Fix index file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { BytebotClient } from "./wrapper/BytebotClient";
+
 export * as Bytebot from "./api";
-export { BytebotClient } from "./Client";
+export { BytebotClient } from "./wrapper/BytebotClient";
 export { BytebotEnvironment } from "./environments";
 export { BytebotError, BytebotTimeoutError } from "./errors";
+
+export default new BytebotClient();
